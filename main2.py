18e82ae79e1f1196be9d65d2f5460648842eec87
teg3r56 <150209511+teg3r56@users.noreply.github.com>
2023-11-08 13:56:22 -0700

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import openai
-import os
 import ast
 import random
 from openai import OpenAI
 
-# initialize api key
-client = OpenAI(api_key=(st.secrets["OPEN_API_KEY"]))
+# Initialize the OpenAI client with your API key
+client = OpenAI(api_key=st.secrets["OPEN_API_KEY"])
 
 def parse_questions(content):
     try:
@@ -24,62 +23,69 @@ def parse_questions(content):
         return None
 
 def generate_questions_from_topic(topic):
-    try:
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {
-                    "role": "system",
-                    "content": "Generate a list of multiple-choice questions with answers and explanations. The output should be in the form of a Python list, with each question represented as a tuple. Each tuple should contain the question text, a list of options, the index of the correct option, and an explanation. There should only be one pair of brackets surrounding the entire list, and no additional brackets around individual tuples."
-                    },
-                {
-                    "role": "user", 
-                    "content": f"Create as many needed multiple-choice questions about {topic}. "
-                                                "In this exact format, only one pair of brackets surrounding all questions: "
-                                                "[('question', ['options', 'options', 'options'], correct_option_index, 'explanation')] "
-                                                "Example: ["
-                                                "('How many valence electrons do elements in the Alkali metal family have?', "
-                                                "['1', '2', '3'], 0, 'Alkali metals belong to Group 1A and have 1 valence electron.'),"
-                                                "('What is the common oxidation state of Alkali metals?', ['+1', '+2', '0'], 0, "
-                                                "'Alkali metals have an oxidation state of +1 as they tend to lose one electron.')]"
-                     }
-            ]
-        )
+    with st.spinner('Generating your quiz...'):
+        try:
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "Generate a list of multiple-choice questions with answers and explanations. The output should be in the form of a Python list, with each question represented as a tuple. Each tuple should contain the question text, a list of options, the index of the correct option, and an explanation. There should only be one pair of brackets surrounding the entire list, and no additional brackets around individual tuples."
+                        },
+                    {
+                        "role": "user", 
+                        "content": f"Create as many needed multiple-choice questions about {topic}. "
+                                                        "In this exact format, only one pair of brackets surrounding all questions: "
+                                                        "[('question', ['options', 'options', 'options'], correct_option_index, 'explanation')] "
+                                                        "Example: ["
+                                                        "('How many valence electrons do elements in the Alkali metal family have?', "
+                                                        "['1', '2', '3'], 0, 'Alkali metals belong to Group 1A and have 1 valence electron.'),"
+                                                        "('What is the common oxidation state of Alkali metals?', ['+1', '+2', '0'], 0, "
+                                                        "'Alkali metals have an oxidation state of +1 as they tend to lose one electron.')]"
+                        }
+                ]
+            )
 
-        content = response.choices[0].message.content.strip()
+            content = response.choices[0].message.content.strip()
 
-        if not content.startswith("[") or not content.endswith("]"):
-            content = "[" + content.replace("]\n\n[", ", ") + "]"
+            if not content.startswith("[") or not content.endswith("]"):
+                content = "[" + content.replace("]\n\n[", ", ") + "]"
 
-        # parse content into a list of questions
-        questions = parse_questions(content)
+            questions = parse_questions(content)
 
-        if questions:
-            random.shuffle(questions)
-            return questions
-        else:
-            st.error("Could not parse the API response into quiz questions.")
-            return None
-    except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
-        return None
-    
+            if questions:
+                random.shuffle(questions)
+                st.session_state.questions = questions
+                st.session_state.current_question_index = 0
+                st.session_state.correct_answers = 0
+                return True
+            else:
+                st.error("Could not parse the API response into quiz questions.")
+                return False
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
+            return False
+
 if 'questions' not in st.session_state:
     st.session_state.questions = []
+    st.session_state.correct_answers = 0
+    st.session_state.current_question_index = 0
 
-def generate_and_store_questions(topic):
-    questions = generate_questions_from_topic(topic)
-    if questions:
-        st.session_state.questions = questions
-        st.session_state.current_question_index = 0
-        
 def main_screen():
     st.title("Teague Coughlin Quiz Generator")
     topic = st.text_input("Enter the topic you want to create a quiz about:")
     generate_quiz = st.button("Generate Quiz")
 
     if generate_quiz and topic:
-        generate_and_store_questions(topic)
+        with st.empty():
+            for percent_complete in range(100):
+                progress = min(1.0, 1 - 1 / (0.1 * percent_complete + 1))
+                st.progress(progress)
+                st.text(f"Loading... {int(100 * progress)}%")
+                time.sleep(0.1)
+        quiz_generated = generate_questions_from_topic(topic)
+        if quiz_generated:
+            st.experimental_rerun()
 
     if 'questions' in st.session_state and st.session_state.questions:
         question_tuple = st.session_state.questions[st.session_state.current_question_index]
@@ -87,19 +93,24 @@ def main_screen():
         st.write(question)
         option = st.radio("Choices", options, key=f"option{st.session_state.current_question_index}")
 
-        if st.button("Submit Answer"):
+        submit_answer = st.button("Submit Answer")
+        if submit_answer:
             if options.index(option) == correct_answer_index:
+                st.session_state.correct_answers += 1
                 st.success("Correct!")
-                if st.session_state.current_question_index < len(st.session_state.questions) - 1:
-                    st.session_state.current_question_index += 1
-                    st.experimental_rerun()
-                else:
-                    st.balloons()
-                    st.session_state.questions = []
-                    st.session_state.current_question_index = 0
-                    st.write("Quiz Finished! Start again?")
+                time.sleep(0.5)
             else:
                 st.error(f"Incorrect! {explanation}")
+            if st.session_state.current_question_index < len(st.session_state.questions) - 1:
+                st.session_state.current_question_index += 1
+            else:
+                st.balloons()
+                st.write(f"Quiz Finished! You got {st.session_state.correct_answers} out of {len(st.session_state.questions)} correct.")
+                if st.button("Restart Quiz"):
+                    st.session_state.questions = []
+                    st.session_state.correct_answers = 0
+                    st.session_state.current_question_index = 0
+                    main_screen()
 
 if __name__ == "__main__":
     main_screen()
